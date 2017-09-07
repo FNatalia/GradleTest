ed70318600a252377b3bf55baba3614943c638ee
Natalia <fishchukova.natalia@gmail.com>
2017-09-07 23:48:04 +0300

    first commit

---
+++ src/test/java/MyTest.java
@@ -0,0 +1,12 @@
+import org.junit.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created on 07.09.2017.
+ */
+public class MyTest {
+    @Test
+    public void testMyTest() throws Exception {
+        Assert.assertTrue(true);
+    }
+}
